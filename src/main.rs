f4f6c4518e47b8f36e2df495ef6a37cdf50eaf8b
ved08 <vedvardhan.g@gmail.com>
2024-10-15 19:10:21 +0530

    basic functionality ready

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+use minigrep::Config;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args)
+    // .unwrap();
+    .unwrap_or_else(|err| {
+        println!("Error: {err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
